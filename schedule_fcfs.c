b297efe0b0b6d9ea4f923b1126328597a8542161
Jnguyenpham94 <55006375+Jnguyenpham94@users.noreply.github.com>
2020-12-06 02:05:52 -0800

    working on schedule() fcfs

--- schedule_fcfs.c
+++ schedule_fcfs.c
@@ -8,5 +8,22 @@
 #include "task.h"
 #include "list.h"
 #include "cpu.h"
+#include "schedulers.h"
+
+struct node newNode;
 
 //Your code and design here
+void add(char *name, int priority, int burst)
+{
+    Task newTask;
+    struct node newNode;
+    newTask.name = name;
+    newTask.priority = priority;
+    newTask.burst = burst;
+    insert(&newNode, &newTask);
+}
+
+void schedule()
+{
+    
+}
