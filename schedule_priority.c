237084cedf8149d01b06b9dfe2ce46c1b6c3795b
Jnguyenpham94 <55006375+Jnguyenpham94@users.noreply.github.com>
2020-12-06 18:21:17 -0800

    basic add() method included need to do schedule

--- schedule_priority.c
+++ schedule_priority.c
@@ -8,7 +8,19 @@
 #include "task.h"
 #include "list.h"
 #include "cpu.h"
+#include "schedulers.h"
+
+Task taskList[10];
+int i = 0;
 
 /*
  * Your code and design here:
  */
+void add(char *name, int priority, int burst)
+{
+    //saves values of the current line of text into the current location
+    taskList[i].name = name;
+    taskList[i].priority = priority;
+    taskList[i].burst = burst;
+    i++;
+}
