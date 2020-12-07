f1647c53d5f113d179fdc69949c9ea80a07e9c52
Jnguyenpham94 <55006375+Jnguyenpham94@users.noreply.github.com>
2020-12-06 21:22:36 -0800

    round robin almost done
    still need to deal with the 0 cases

--- schedule_rr.c
+++ schedule_rr.c
@@ -11,8 +11,10 @@
 #include "cpu.h"
 #include "schedulers.h"
 
-Task taskList[10];
+Task taskList[6];
+int length = sizeof(taskList) / sizeof(taskList[0]); //size of tasklist
 int i = 0;
+int count = 6;
 
 /*
  * Your code and design here:
@@ -24,4 +26,28 @@ void add(char *name, int priority, int burst)
     taskList[i].priority = priority;
     taskList[i].burst = burst;
     i++;
+}
+
+void schedule()
+{
+    while(count > 0)
+    {
+        for (int j = 0; j < length; j++)
+        { //outputs a quantum time until all burst values are 0
+            if (taskList[j].burst == 0)
+            {
+                count--;
+            }
+            if (taskList[j].burst >= QUANTUM)
+            {
+                run(&taskList[j], QUANTUM);
+                taskList[j].burst = taskList[j].burst - QUANTUM;
+            }
+            if (taskList[j].burst < QUANTUM)
+            {
+                run(&taskList[j], taskList[j].burst);
+                taskList[j].burst = 0;
+            }
+        }
+    }
 }
