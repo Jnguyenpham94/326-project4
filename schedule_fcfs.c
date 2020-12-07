0983245667db4ed594b82f810ab7de2ae27f6ffd
Jnguyenpham94 <55006375+Jnguyenpham94@users.noreply.github.com>
2020-12-06 18:18:48 -0800

    fcfs complete

--- schedule_fcfs.c
+++ schedule_fcfs.c
@@ -10,20 +10,23 @@
 #include "cpu.h"
 #include "schedulers.h"
 
-struct node newNode;
+Task taskList[10];
+int i = 0;
 
 //Your code and design here
 void add(char *name, int priority, int burst)
 {
-    Task newTask;
-    struct node newNode;
-    newTask.name = name;
-    newTask.priority = priority;
-    newTask.burst = burst;
-    insert(&newNode, &newTask);
+    //saves values of the current line of text into the current location
+    taskList[i].name = name;
+    taskList[i].priority = priority;
+    taskList[i].burst = burst;
+    i++;
 }
 
 void schedule()
 {
-    
+    int length = sizeof(taskList)/sizeof(taskList[0]);//size of tasklist
+    for(int j=0; j < length; j++){//just outputs the taskList without any special priority stuff
+        run(&taskList[j], taskList[j].burst);
+    }
 }
